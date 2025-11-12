00ed158d729702b4a403afc07ce3e88ffdcec853
Amaanuddin05 <amaanuddin0501@gmail.com>
2025-11-12 05:32:23 +0530

    basic navbar component done

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgFor],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+   isDarkMode = false;
+  
+  navLinks = [
+    { label: 'Home', path: '/home' },
+    { label: 'Fetch', path: '/fetch' },
+    { label: 'Memory Search', path: '/memory-search' },
+    { label: 'History', path: '/history' },
+    { label: 'Analyze', path: '/analyze' },
+    { label: 'Chat', path: '/chat' }
+  ];
+
+  userAvatar = 'https://lh3.googleusercontent.com/aida-public/AB6AXuCzG7BAa8uOgl55IHGxRZ1Xds1iSjzcjWnNpu9WntW-QbZIIV8B1gjdC31CI0F0AMR7jYv6qlroSZ-D9ZFM3ZLea1gjHAIM3tkVu4kmmM7Cgx_rPlzMnZ07Ir4TYMDDNuT3IDyr5fUiHYAFrj2jFiaajClaNESRu-qnT9Ky6vGvn3f24ZfAGhx_lYSXq6S4veegxjxjC30sEYqSSoaOmm-67pJKIn6PQzMHAwMLe2YcpU7IhIQmDsZVoKgB_mx4XQxgteIyWkdi4II';
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    // Add your theme toggle logic here
+    console.log('Dark mode:', this.isDarkMode);
+  }
+}
