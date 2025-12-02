b9a654efe1c524d2ead6f01a6ad72f8c012a4581
Amaanuddin05 <amaanuddin0501@gmail.com>
2025-12-02 22:16:55 +0530

    removed app.js and add start script (stable)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
-import app from "./app.js";
+import express from "express";
+import cors from "cors";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import fetchRoutes from "./routes/fetchRoutes.js";
+import summaryRoutes from "./routes/summaryRoutes.js";
+import downloadRoutes from "./routes/downloadRoutes.js";
 
+const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(uploadRoutes);
+app.use(fetchRoutes);
+app.use(summaryRoutes);
+app.use(downloadRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
